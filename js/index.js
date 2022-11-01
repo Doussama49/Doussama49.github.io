ddca17afefdb5e9ba89af56206bbb44cd4f0b8d2
Doussama49 <116646891+Doussama49@users.noreply.github.com>
2022-11-01 02:37:04 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,11 +8,9 @@
 
   //fonction map pour traiter toutes les valeurs du tableau (donc toutes les stations)
   stationsTab.map((station) => {
-    const stationTab = station.split(";"); 
+    const stationTab = station.split(";"); // regarde la methode split sur internet, ça sépare fonction d'un caractère en gros
     const id = stationTab[0].substring(2, 6); //id de la ligne
     const stationName = stationTab[0].substring(7, stationTab[0].length); //nom de la station
-    //console.log(stationName);
-    //traceAllPoint(stationName);
     const ligne = ` (ligne ${stationTab[1].trim()})`; // numéro de la ligne
     const optionElement = document.createElement("option"); // element à ajouter dans départ
     const optionBis = document.createElement("option"); // element à ajouter dans arrivée
@@ -23,7 +21,6 @@
     elementSelectDepart.append(optionBis); // ajout de l'element dans départ
     elementSelectArrivee.append(optionElement); // ajout de l'element dans arrivée
   });
-traceAllPoint();
 })();
 
 async function getStations() {
@@ -34,54 +31,6 @@ async function getStations() {
   return stations.filter(
     (station) => station.startsWith("V") && station.length < 136
   );
-}
-
-async function traceAllPoint(){
-const groupeStation = document.getElementById("stations");
-const aAjouter = [];
-
-const requete = await fetch("pospoints.txt");
-const texte = await requete.text();
-
-const stations = texte.split("\r\n"); // chaque ligne
-for (const ligne of stations) {
-    const resultat = ligne.split(";");
-    const posX = resultat[0];
-    const posY = resultat[1];
-    const nomStation = resultat[2].replace(/@/g, " ");
-    var nom = nomStation;
-    aAjouter.push(`<circle style ="fill: white; stroke: black; stroke-width: 2px;" cx="${posX}" cy="${posY}" r="5"></circle>`);
-    
-}
-  groupeStation.innerHTML = aAjouter.join();
-}
-
-async function tracePoint(sommet){
-  const groupeStation = document.getElementById("stations");
-const aAjouter = [];
-
-const requete = await fetch("pospoints.txt");
-const texte = await requete.text();
-
-const stations = texte.split("\r\n"); // chaque ligne
-for (const ligne of stations) {
-    const resultat = ligne.split(";");
-    const posX = resultat[0];
-    const posY = resultat[1];
-    const nomStation = resultat[2].replace(/@/g, " ");
-
-    if (nomStation==sommet){
-      aAjouter.push(`<circle cx="${posX}" cy="${posY}" r="2"></circle>`);
-    }
-}
-groupeStation.innerHTML = aAjouter.join();
-}
-
-async function connexe(){
-  const requete = await fetch(`https://oussama.teroaz.me/connexe`);
-    // traduction du json en javascript
-    const resultat = await requete.text();
-  alert(resultat);
 }
 
 async function handleClick() {
@@ -122,15 +71,15 @@ async function handleClick() {
         const duree = resultat[resultat.length - 1];
         const str1 = findFullString(val[1]);
         const str3 = findFullString(val[3]);
-        if (index === 0) str1.then((val) => ajouterText("Départ de : " + val));
+        if (index === 0) str1.then((val) => ajouterText("Départ : " + val));
         else if (val[0] === val[2])
-          str3.then((val) => ajouterText("<br>Changement à : " + val));
+          str3.then((val) => ajouterText("Changement : " + val));
         else if (index === resultat.length - 2)
           str3.then((val) =>
             ajouterText(
-              "<br>Arrivée à : " +
+              "Arrivée : " +
                 val +
-                "<br><br> Durée : " +
+                "<br> Durée : " +
                 Math.floor(duree / 60 / 60) +
                 " heures " +
                 Math.floor(duree / 60) +
@@ -139,7 +88,7 @@ async function handleClick() {
                 " secondes"
             )
           );
-        else str1.then((val) => ajouterText("-"+val.split("(")[0]));
+        else str1.then((val) => ajouterText(val.split("(")[0]));
       }
     });
     //ça met le texte renvoyé par la requête dans un h4
@@ -147,6 +96,12 @@ async function handleClick() {
   //remise à 0 des elements select
   elementArrivee.selectedIndex = 0;
   elementDepart.selectedIndex = 0;
+}
+async function connexe(){
+  const requete = await fetch(`https://oussama.teroaz.me/connexe`);
+    // traduction du json en javascript
+    const resultat = await requete.text();
+  alert(resultat);
 }
 
 async function findFullString(stationId) {
@@ -169,6 +124,3 @@ function ajouterText(text) {
   element.innerHTML += doTab ? "&emsp;&emsp;" : "";
   element.innerHTML += text + "<br>";
 }
-
-
-
